7b207de41edbfa07aa18631dc4a53e240fb6f248
Anna Manole <am224wd@student.lnu.se>
2023-11-16 16:35:36 +0100

    REFACTOR - make the name property private

--- src/Herb.ts
+++ src/Herb.ts
@@ -1,7 +1,11 @@
 export class Herb {
-  name: string
+  #name: string
 
   constructor(name: string) {
-    this.name = name
+    this.#name = name
+  }
+
+  get name(): string {
+    return this.#name
   }
 }
