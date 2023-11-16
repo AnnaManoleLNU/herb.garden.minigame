1b1c8563d96918e043c9c3aea5bce40c2aae09d5
Anna Manole <am224wd@student.lnu.se>
2023-11-16 16:54:53 +0100

    GREEN - name length check in constructor

--- src/Herb.ts
+++ src/Herb.ts
@@ -2,6 +2,9 @@ export class Herb {
   #name: string
 
   constructor(name: string) {
+    if (name.length > 50) {
+      throw new Error('Name must be less than 50 characters long.')
+    }
     this.#name = name
   }
 
