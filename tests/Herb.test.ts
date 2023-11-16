7c2c87b9a4a632aec4ea353997288b9e3aff6c01
Anna Manole <am224wd@student.lnu.se>
2023-11-16 16:53:34 +0100

    RED - name length test

--- tests/Herb.test.ts
+++ tests/Herb.test.ts
@@ -1,6 +1,11 @@
-import { Herb } from '../src/Herb';
+import { Herb } from "../src/Herb";
 
-test('a herb should be created with name', () => {
-  const herb = new Herb('Thyme')
-  expect(herb.name).toEqual('Thyme')
+test("a herb should be created with name", () => {
+  const herb = new Herb("Thyme");
+  expect(herb.name).toEqual("Thyme");
+})
+
+test("name should throw exception if longer than 50 characters", () => {
+  const longName = "Looooooooooooooooooooooooooooooooooooong alligator pepper";
+  expect(() => new Herb(longName)).toThrow(Error);
 })
