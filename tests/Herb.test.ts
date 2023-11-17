f90ebeb54b42b155cbd20e1611fbd7a283ccbcee
Anna Manole <am224wd@student.lnu.se>
2023-11-17 16:39:25 +0100

    RED - Herb name attribte should match enum

--- tests/Herb.test.ts
+++ tests/Herb.test.ts
@@ -1,6 +1,13 @@
-import { Herb } from "../src/Herb"
+import { Herb , HerbName } from "../src/Herb"
 
 test("herb name not part of the HerbName enum should throw an error", () => {
   const invalidHerbName = "Dill"
   expect(() => new Herb(invalidHerbName)).toThrow(Error)
 })
+
+test("herb's name attribute should match the exact string value of the HerbName enum", () => {
+  const herbName = "thyme"
+  const herb = new Herb(herbName)
+  
+  expect(herb.name).toBe(HerbName.Thyme)
+});
