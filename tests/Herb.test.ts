efe3f4bb85f6500c27b6ee9a5a165381a25bf874
Anna Manole <am224wd@student.lnu.se>
2023-11-20 16:29:47 +0100

    RED - throw error on watering needs

--- tests/Herb.test.ts
+++ tests/Herb.test.ts
@@ -4,3 +4,7 @@ test ("Herb watering needs should be not be less than 1", () => {
   const herb = new Herb(HerbName.Thyme , 0)
   expect(herb.wateringNeeds).toBeGreaterThanOrEqual(1)
 })
+
+test("Herb should throw an error if watering needs is less than 1", () => {
+  expect(() => new Herb(HerbName.Thyme, 0)).toThrow("Watering needs cannot be less than 1")
+})
