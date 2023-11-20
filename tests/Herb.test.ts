6f6f1faf3a7e5eba8cfbb04b964c18140c85a574
Anna Manole <am224wd@student.lnu.se>
2023-11-20 16:33:28 +0100

    RED - too high watering needs

--- tests/Herb.test.ts
+++ tests/Herb.test.ts
@@ -3,3 +3,7 @@ import { Herb , HerbName } from "../src/Herb"
 test("Herb should throw an error if watering needs is less than 1", () => {
   expect(() => new Herb(HerbName.Thyme, 0)).toThrow("Watering needs cannot be less than 1")
 })
+
+test("Herb should throw an error if watering needs is higher than 3", () => {
+  expect(() => new Herb(HerbName.Thyme, 4)).toThrow("Watering needs cannot be higher than 3")
+})
