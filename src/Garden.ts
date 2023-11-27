0abe12fd2c4a9838a50bc613138db4cc2b476d7a
Anna Manole <am224wd@student.lnu.se>
2023-11-27 15:02:28 +0100

    RED - should have Thyme

--- src/Garden.ts
+++ src/Garden.ts
@@ -1,7 +1,12 @@
 import { Herb } from "./Herb.ts"
+import { HerbName, OptimalHarvestingTime, WateringNeeds } from "./Herb.ts"
 
 export class Garden {
   #herbs: Herb[] = []
+
+  constructor() {
+    this.#herbs.push(new Herb(HerbName.Basil, WateringNeeds.Low, OptimalHarvestingTime.Day2))
+  }
 
   get herbs(): Herb[] {
     return this.#herbs
--- tests/Garden.test.ts
+++ tests/Garden.test.ts
@@ -2,11 +2,19 @@ import { Garden } from '../src/Garden.ts'
 import { Herb } from '../src/Herb.ts'
 
 describe("Garden class", () => {
+
   test("should have an array list of herbs", () => {
     const garden = new Garden()
     for (let i = 0; i < garden.herbs.length; i++) {
       expect(garden.herbs[i]).toBeInstanceOf(Herb)
     }
   })
-  
+
+  test("should have Thyme", () => {
+    const garden = new Garden()
+    for (let i = 0; i < garden.herbs.length; i++) {
+      expect(garden.herbs[i].name).toBe("Thyme")
+    }
+  })
+
 })
