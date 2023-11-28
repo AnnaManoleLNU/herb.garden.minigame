7ba95561af346b6be259c2b4a9c2d28991d93051
Anna Manole <am224wd@student.lnu.se>
2023-11-28 09:05:11 +0100

    REFACTOR

--- tests/Garden.test.ts
+++ tests/Garden.test.ts
@@ -1,11 +1,16 @@
 import { Garden } from '../src/Garden.ts'
 import { Herb } from '../src/Herb.ts'
 
-describe("Garden class", () => {
+let herbs: Array<Herb>
+let garden: Garden
 
+beforeEach(() => {
+  herbs = new Array<Herb>()
+  garden = new Garden(herbs)
+})
+
+describe("Garden class", () => {
   test("should have an array list of herbs", () => {
-    const herbs = new Array<Herb>()
-    const garden = new Garden(herbs)
     for (let i = 0; i < garden.herbs.length; i++) {
       expect(garden.herbs[i]).toBeInstanceOf(Herb)
     }
@@ -16,17 +21,12 @@ describe("Garden class", () => {
   hasHerb("Rosemary")
 
   test("should have an array list of herbs in the constructor", () => {
-    const herbs = new Array<Herb>()
-    const garden = new Garden(herbs)
     expect(garden.herbs).toBe(herbs)
   })
-
 })
 
 function hasHerb(herbName: string) {
   test(`should have ${herbName}`, () => {
-    const herbs = new Array<Herb>()
-    const garden = new Garden(herbs)
     const herbNames = garden.herbs.map(herb => herb.name)
     expect(herbNames).toContain(herbName)
   })
