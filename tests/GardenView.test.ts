74b8bedb1b840ffcf591a3107c09440aecdf3edf
Anna Manole <am224wd@student.lnu.se>
2023-12-04 15:14:06 +0100

    REFACTOR

--- tests/GardenView.test.ts
+++ tests/GardenView.test.ts
@@ -1,11 +1,10 @@
-import { GardenView } from "../src/view/GardenView"
+import { GardenView } from '../src/view/GardenView'
 
-describe("GardenView class", () => {
-  test("should display a welcome message" , () => {
+describe('GardenView class', () => {
+  test('should display a welcome message', () => {
     const gardenView = new GardenView()
     console.log = jest.fn()
     gardenView.displayWelcomeMessage()
-
     expect(console.log).toHaveBeenCalledWith('Welcome to the Garden!')
   })
 })
