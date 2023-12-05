aa65f83c664e92d5f63af4e11a293f182d4e975d
Anna Manole <am224wd@student.lnu.se>
2023-12-05 11:43:58 +0100

    REFACTOR

--- tests/Player.test.ts
+++ tests/Player.test.ts
@@ -7,21 +7,21 @@ beforeEach(() => {
 })
 
 describe('Player class score', () => {
-  test('should have a score of 0 when created', () => {
+  test('should be 0 when created', () => {
     expect(player.score).toBe(0)
   })
 
-  test('score should increase by 3 when passing in 3 (max herb quality)', () => {
+  test('should increase by 3 when passing in 3 (max herb quality)', () => {
     player.addScore(3)
     expect(player.score).toBe(3)
   })
 
-  test('adding in a negative value should not change the score', () => {
+  test('should not change when adding a negative value', () => {
     player.addScore(-3)
     expect(player.score).toBe(0)
   })
 
-  test('max score should be 36 (6 herbs with 6 points to get from watering perfecty and harvesting perfectly)', () => {
+  test('should be 36 max (6 herbs with 6 points to get from watering perfecty and harvesting perfectly)', () => {
     const score = 36
     player.addScore(score)
     const illegalPoint = 1
@@ -29,17 +29,17 @@ describe('Player class score', () => {
     expect(player.score).toBe(score)
   })
 
-  test('adding points that exceeds max score should not change the score', () => {
+  test('should not change the score when ading points that exceeds 36 (max score)', () => {
     player.addScore(35)
     player.addScore(2)
     expect(player.score).toBe(35)
   })
 
-  test('adding a float to the score should throw exception', () => {
+  test('should throw exception when adding a float to it', () => {
     expect(() => player.addScore(1.5)).toThrow()
   })
 
-  test('adding a float to the score should throw an exception and not change the score', () => {
+  test('should throw an exception and not change the score when adding a float to it', () => {
     const initialScore = player.score
     expect(() => player.addScore(1.5)).toThrow()
     expect(player.score).toBe(initialScore)
