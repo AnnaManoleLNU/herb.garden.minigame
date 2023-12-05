a7067f601c3cbb6f09174e86c6ea99799c357ef8
Anna Manole <am224wd@student.lnu.se>
2023-12-05 10:30:13 +0100

    REFACTOR

--- tests/Player.test.ts
+++ tests/Player.test.ts
@@ -1,31 +1,32 @@
 import { Player } from '../src/model/Player'
 
+let player: Player
+
+beforeEach(() => {
+  player = new Player()
+})
+
 describe('Player class', () => {
   test('should have a score of 0 when created', () => {
-    const player = new Player()
     expect(player.score).toBe(0)
   })
 
   test('score should increase by 3 when passing in 3 (max herb quality)', () => {
-    const player = new Player()
     player.addPoint(3)
     expect(player.score).toBe(3)
   })
 
   test('adding in a negative value should not change the score', () => {
-    const player = new Player()
     player.addPoint(-3)
     expect(player.score).toBe(0)
   })
 
-  test('max score should be 6 x 6', () => {
-    const player = new Player()
-    // 6 herbs, 6 max quality each (3 from watering perfectly, 3 from harvesting perfectly)
-    const score = 6 * 6 
+  test('max score should be 36 (6 herbs with 6 points to get from watering perfecty and harvesting perfectly)', () => {
+    const score = 36
     player.addPoint(score)
     const illegalPoint = 1
     player.addPoint(illegalPoint)
     expect(player.score).toBe(score)
-})
+  })
 })
 
