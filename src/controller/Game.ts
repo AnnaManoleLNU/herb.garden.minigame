f1828bef8dbea0987ff111902f3d1244d386031a
Anna Manole <am224wd@student.lnu.se>
2023-12-06 09:54:47 +0100

    GREEN

--- src/controller/Game.ts
+++ src/controller/Game.ts
@@ -1,12 +1,15 @@
 import { Player } from '../model/Player'
 import { Garden } from '../model/Garden'
+import { GardenView } from '../view/GardenView'
 
 export class Game {
   player: Player
   garden: Garden
+  view: any
 
   constructor() {
     this.player = new Player()
     this.garden = new Garden()
+    this.view = new GardenView(this.garden)
   }
 }
